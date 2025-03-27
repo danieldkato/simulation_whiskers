774ad188e5d2ca5e95ff4a98cedcd502f07f5635
Daniel D Kato <daniel.d.kato@gmail.com>
2025-03-27 10:04:12 -0400

    Define base_output_directory based on which machine script is run on

--- scripts/iterate_ae.py
+++ scripts/iterate_ae.py
@@ -98,7 +98,7 @@ task_defs = [
 
 
 # Define general variables:
-n_files = 8
+n_files = 1
 n_geo_subsamples = 1
 sum_inpt=False
 xor=True
@@ -158,7 +158,10 @@ n_predicted_bins = 4
 gpu = False
 
 # Output directory:
-base_output_directory='E:\\simulation_whiskers\\results\\'
+if hostname == 'DESKTOP-PJOJHT':
+    base_output_directory = os.path.join('Z:\\', 'users', 'Dan', 'code', 'ws', 'results')
+else:
+    base_output_directory = 'E:\\simulation_whiskers\\results\\'
 run_base_name='run'
 sv=False
   
